f6af4ccdcfbc472d9b79b8b3a128cc4ca5b217c0
Paul M. Rodriguez <pmr@ruricolist.com>
2013-12-14 12:33:17 -0600

    Transfer updated_parsed, published_parsed

--- parser.py
+++ parser.py
@@ -19,20 +19,24 @@ def ts_to_universal (ts):
 def struct_time_p(obj):
     return isinstance(obj, time.struct_time)
 
+def transfer_date (e, e2, key):
+    date = e[key]
+    if struct_time_p(date):
+        e2[key] = ts_to_universal(date)
+    return e2
+
 def simplify_entry(e):
     e2 = {key: e.get(key) for key in item_keys}
     e2['link'] = e.get('feedburner_origlink') or e.get('link')
-    pubdate = e.get('updated_parsed') or e.get('published_parsed')
-    if struct_time_p(pubdate):
-        e2['pubdate'] = ts_to_universal(pubdate)
+    transfer_date(e, e2, 'updated_parsed')
+    transfer_date(e, e2, 'published_parsed')
     return e2
 
 def simplify_feed (d):
     d2 = {key: d.feed.get(key) for key in feed_keys}
     d2['version'] = d['version'];
-    pubdate = d.feed.get('updated_parsed') or d.feed.get('published_parsed')
-    if struct_time_p(pubdate):
-        d2['pubdate'] = ts_to_universal(pubdate)
+    transfer_date(d, d2, 'updated_parsed')
+    transfer_date(d, d2, 'published_parsed')
     d2['entries'] = [simplify_entry(e) for e in d.entries[:10]]
     return d2
 
