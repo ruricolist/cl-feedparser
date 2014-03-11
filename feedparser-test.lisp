71b11b425e07a5703c18e1206f5a0a3f61e33071
Paul M. Rodriguez <pmr@ruricolist.com>
2014-03-11 00:26:48 -0500

    Dead code.

--- feedparser-test.lisp
+++
@@ -1,5 +0,0 @@
-(in-package :cl-feedparser)
-
-(def-suite cl-feedparser)
-
-(in-suite cl-feedparser)
--- parse.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-# A separate file so parser.py is always compiled.
-
-import parser
-
-parser.parse_stdin_feed()
